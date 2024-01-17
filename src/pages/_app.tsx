95b3ff207d6c4942d430597a0744b75887631ca6
ih.zonaid <ahammedzonaid@gmail.com>
2024-01-17 19:40:47 +0600

    prisma scheme added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,18 @@ import { type AppType } from "next/app";
 
 import { api } from "~/utils/api";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import dynamic from "next/dynamic";
+import { Titillium_Web } from "next/font/google";
 import "~/styles/globals.css";
+
+const queryClient = new QueryClient();
+
+const PopupImports = dynamic(
+  () => import("package/connect_wallet/src/components/popup_imports"),
+);
+
+const inner = Titillium_Web({ subsets: ["latin"], weight: "400" });
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,7 +23,10 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+        <PopupImports className={inner.className} />
+      </QueryClientProvider>
     </SessionProvider>
   );
 };
