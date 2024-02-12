ced8a810705fa8c863cae82281ac4aa3eed87ba6
ih.zonaid <ahammedzonaid@gmail.com>
2024-02-12 17:17:36 +0600

    show connect wallet when no login in.

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,14 +3,25 @@ import LeftBar from "./left-sidebar";
 import RightBar from "./right-sidebar";
 import BottomNav from "./mobile/bottom-nav";
 import TopNav from "./mobile/top-nav";
+import {
+  ConnectWalletButton,
+  useConnectWalletStateStore,
+} from "package/connect_wallet";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const { isAva } = useConnectWalletStateStore();
   return (
     <div className="flex">
       <LeftBar />
       <div className="h-screen flex-1 overflow-y-auto  scrollbar-hide">
         <TopNav />
-        {children}
+        {isAva ? (
+          <>{children}</>
+        ) : (
+          <div className="flex h-full items-center justify-center">
+            <ConnectWalletButton />
+          </div>
+        )}
         <BottomNav />
       </div>
       <RightBar />
