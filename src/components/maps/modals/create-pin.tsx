bbd5c7a52adea13b1b46f8ce790618d0b18988fb
Zonaid <ahammedzonaid@gmail.com>
2024-06-28 00:24:32 +0600

    fix step

--- src/components/maps/modals/create-pin.tsx
+++ src/components/maps/modals/create-pin.tsx
@@ -494,7 +494,7 @@ export default function CreatePinModal({
             <label className="text-sm font-medium">Latitude</label>
             <input
               type="number"
-              step={0.0000000000000001}
+              step={0.0000000000000000001}
               {...register("lat", { valueAsNumber: true })}
               className="input input-bordered"
             />
@@ -503,7 +503,7 @@ export default function CreatePinModal({
           <div className="flex flex-col space-y-2">
             <label className="text-sm font-medium">Longitude</label>
             <input
-              step={0.00000000000000001}
+              step={0.0000000000000000001}
               type="number"
               {...register("lng", { valueAsNumber: true })}
               className="input input-bordered"
