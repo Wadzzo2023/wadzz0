840940a2054bfac0a9701bdb93fd3a368b8a573b
Zonaid <ahammedzonaid@gmail.com>
2024-07-11 13:26:36 +0600

    updated game location 4 public & following pins

--- src/pages/api/game/locations/index.ts
+++ src/pages/api/game/locations/index.ts
@@ -21,7 +21,7 @@ export default async function handler(
 
   // Return the locations
 
-  const db_locations = await db.location.findMany({
+  const all_creator_public_pin = await db.location.findMany({
     select: {
       id: true,
       latitude: true,
@@ -40,10 +40,51 @@ export default async function handler(
     where: {
       approved: { equals: true },
       endDate: { gte: new Date() },
+      tier: { is: null },
     },
   });
 
-  const locations: Location[] = db_locations.map((location) => {
+  const user = await db.user.findUnique({
+    where: { id: session.user.id },
+    select: {
+      followings: {
+        include: {
+          creator: {
+            include: {
+              Location: {
+                include: {
+                  consumers: {
+                    select: { userId: true },
+                    where: { userId: session.user.id },
+                  },
+                  creator: true,
+                },
+                where: {
+                  approved: { equals: true },
+                  endDate: { gte: new Date() },
+                  tier: {
+                    isNot: null,
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+
+  const following_pins = user?.followings.map((el) => {
+    const pins = el.creator.Location.map((location) => location);
+    return pins;
+  });
+
+  const allFollowingPins = user?.followings.flatMap(
+    (el) => el.creator.Location,
+  );
+
+  // let locations: Location[];
+  const followingsLocations = allFollowingPins?.map((location) => {
     return {
       id: location.id,
       lat: location.latitude,
@@ -59,8 +100,33 @@ export default async function handler(
       brand_image_url:
         location.creator.profileUrl ?? "https://picsum.photos/100/100",
       brand_id: location.creatorId,
+      public: false,
     };
   });
+
+  let locations: Location[] = all_creator_public_pin.map((location) => {
+    return {
+      id: location.id,
+      lat: location.latitude,
+      lng: location.longitude,
+      title: location.title,
+      description: location.description ?? "No description provided",
+      brand_name: location.creator.name,
+      url: "https://picsum.photos/200/300",
+      image_url: location.image ?? "https://picsum.photos/500/500",
+      collected: location.consumers.length > 0,
+      collection_limit_remaining: 3,
+      auto_collect: location.autoCollect,
+      brand_image_url:
+        location.creator.profileUrl ?? "https://picsum.photos/100/100",
+      brand_id: location.creatorId,
+      public: true,
+    };
+  });
+
+  if (followingsLocations) {
+    locations = [...locations, ...followingsLocations];
+  }
 
   res.status(200).json({ locations: locations });
 }
