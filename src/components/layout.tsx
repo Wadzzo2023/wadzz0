7013e7f7a0d177fdd77c5413419d06de05e4c0f2
Zonaid <ahammedzonaid@gmail.com>
2024-07-17 23:24:04 +0600

    fix layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,9 +4,9 @@ import { useSession } from "next-auth/react";
 import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
 import React from "react";
-import { api } from "~/utils/api";
-import ModalProvider from "./providers/modal-provider";
 import { ThemeProvider } from "./providers/theme-provider";
+import ModalProvider from "./providers/modal-provider";
+import { api } from "~/utils/api";
 // import Header from "./header";
 // import RightDialog from "./right_dialog";
 
@@ -56,47 +56,45 @@ export default function Layout({
 
           <div className="flex-1 overflow-auto bg-base-100/50">
             <div className="flex h-full border-t-2">
-              <LeftBar className="hidden md:flex" />
-              <div id="ih" className="flex-1 border-x-2 ">
-                <LeftBar className="hidden xl:flex" />
-                <div
-                  id="ih"
-                  className="flex-1 border-x-2"
-                  style={
-                    router.pathname.includes("/fans/creator") && creator.data
-                      ? {
-                          background: `url("${creator.data.backgroundSVG}")`,
-                          backgroundSize: "10%",
-                          animation: "pan 135s linear infinite",
-                        }
-                      : {
-                          background: `url("images/guitar.svg")`,
-                          backgroundSize: "10%",
-                          animation: "pan 135s linear infinite",
-                        }
-                  }
-                >
-                  <div className=" h-full overflow-y-auto bg-base-100/80 scrollbar-hide">
-                    {session.status == "authenticated" ? (
-                      <>
-                        <ModalProvider />
-                        {children}
-                      </>
-                    ) : (
-                      <div className="flex h-full items-center justify-center">
-                        <ConnectWalletButton />
-                      </div>
-                    )}
-                    <div className="h-44 " />
-                  </div>
+              <LeftBar className="hidden xl:flex" />
+              <div
+                id="ih"
+                className="flex-1 border-x-2"
+                style={
+                  router.pathname.includes("/fans/creator") && creator.data
+                    ? {
+                        background: `url("${creator.data.backgroundSVG}")`,
+                        backgroundSize: "10%",
+                        animation: "pan 135s linear infinite",
+                      }
+                    : {
+                        background: `url("images/guitar.svg")`,
+                        backgroundSize: "10%",
+                        animation: "pan 135s linear infinite",
+                      }
+                }
+              >
+                <div className=" h-full overflow-y-auto bg-base-100/80 scrollbar-hide">
+                  {session.status == "authenticated" ? (
+                    <>
+                      <ModalProvider />
+                      {children}
+                    </>
+                  ) : (
+                    <div className="flex h-full items-center justify-center">
+                      <ConnectWalletButton />
+                    </div>
+                  )}
+                  <div className="h-44 " />
+                  {/* <BottomNav /> */}
                 </div>
-
-                {router.pathname !== "/walletBalance" &&
-                  router.pathname !== "/settings" &&
-                  router.pathname !== "/about" &&
-                  router.pathname !== "/privacy" &&
-                  session.status == "authenticated" && <RightSideBar />}
               </div>
+
+              {router.pathname !== "/walletBalance" &&
+                router.pathname !== "/settings" &&
+                router.pathname !== "/about" &&
+                router.pathname !== "/privacy" &&
+                session.status == "authenticated" && <RightSideBar />}
             </div>
           </div>
           <RightDialog />
