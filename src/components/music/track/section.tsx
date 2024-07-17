be20e2fcde382ce0fcf7c1b3b100f36461fca8a5
Zonaid <ahammedzonaid@gmail.com>
2024-07-18 02:42:54 +0600

    ..

--- src/components/music/track/section.tsx
+++ src/components/music/track/section.tsx
@@ -17,10 +17,9 @@ export default function TrackSection({ header, playable }: TrackSectionProp) {
     return (
       <div>
         <h3 className="py-2 text-2xl font-bold">{header}</h3>
-        {songs.data &&
-          songs.data.map((song) => (
-            <MusicItem key={song.id} item={song} playable={playable} />
-          ))}
+        {songs.data.map((song) => (
+          <MusicItem key={song.id} item={song} playable={playable} />
+        ))}
 
         <div className="flex flex-col gap-2"></div>
       </div>
