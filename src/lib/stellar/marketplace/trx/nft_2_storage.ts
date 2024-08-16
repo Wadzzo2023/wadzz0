cd3880511363b0105ae42825564a1faeb1b7027f
Action <admin@actn.xyz>
2024-08-16 14:51:42 +0600

    update tostorage trx handle token trust

--- src/lib/stellar/marketplace/trx/nft_2_storage.ts
+++ src/lib/stellar/marketplace/trx/nft_2_storage.ts
@@ -11,6 +11,7 @@ import { networkPassphrase } from "../constant";
 import { SignUserType, WithSing } from "../../utils";
 import { env } from "~/env";
 import { PLATFORM_ASSET, PLATFORM_FEE } from "../../constant";
+import { getplatformAssetNumberForXLM } from "../../fan/get_token_price";
 
 export async function sendNft2StorageXDR({
   assetCode,
@@ -31,32 +32,75 @@ export async function sendNft2StorageXDR({
   const asset = new Asset(assetCode, issuerPub);
   const server = new Horizon.Server(STELLAR_URL);
 
+  const mother = Keypair.fromSecret(env.MOTHER_SECRET);
+
   const transactionInializer = await server.loadAccount(userPub);
+
+  // check storage trust to token
+  const storage = await server.loadAccount(storagePub);
+  const balances = storage.balances;
+  const trust = balances.find((balance) => {
+    if (
+      balance.asset_type === "credit_alphanum12" ||
+      balance.asset_type === "credit_alphanum4"
+    ) {
+      if (
+        balance.asset_code === assetCode &&
+        balance.asset_issuer === issuerPub
+      ) {
+        return true;
+      }
+    }
+  });
+
+  let trxCharge = Number(PLATFORM_FEE);
 
   const Tx2 = new TransactionBuilder(transactionInializer, {
     fee: BASE_FEE,
     networkPassphrase,
-  })
-    .addOperation(
+  });
+
+  if (trust === undefined) {
+    // send required xlm to the storage acc.
+    Tx2.addOperation(
       Operation.payment({
-        destination: Keypair.fromSecret(env.MOTHER_SECRET).publicKey(),
-        amount: PLATFORM_FEE,
-        asset: PLATFORM_ASSET,
-      }),
-    )
-    //
-    .addOperation(
-      Operation.payment({
+        amount: "0.5",
+        asset: Asset.native(),
         destination: storagePub,
-        amount: assetAmount, //copy,
-        asset: asset,
-        source: userPub,
+        source: mother.publicKey(),
       }),
-    )
+    );
+
+    const requiredAsset2refundXlm = await getplatformAssetNumberForXLM(0.5);
+    trxCharge += requiredAsset2refundXlm;
+  }
+
+  Tx2.addOperation(
+    Operation.payment({
+      destination: mother.publicKey(),
+      amount: trxCharge.toString(),
+      asset: PLATFORM_ASSET,
+    }),
+  );
+  // if storage don't have trust
+
+  // make payment
+  const buildTrx = Tx2.addOperation(
+    Operation.payment({
+      destination: storagePub,
+      amount: assetAmount, //copy,
+      asset: asset,
+      source: userPub,
+    }),
+  )
     .setTimeout(0)
     .build();
 
-  return await WithSing({ xdr: Tx2.toXDR(), signWith });
+  if (trust === undefined) {
+    buildTrx.sign(mother);
+  }
+
+  return await WithSing({ xdr: buildTrx.toXDR(), signWith });
 }
 
 export async function sendNftback({
