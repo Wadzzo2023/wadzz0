83cd5052775c2e0b82f8236425c96a1336a97ce7
Action <admin@actn.xyz>
2024-08-25 00:25:52 +0600

    signup page

---
+++ src/pages/signup.tsx
@@ -0,0 +1,10 @@
+import { ConnectWalletButton } from "package/connect_wallet";
+import React from "react";
+
+export default function SignUP() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <ConnectWalletButton />
+    </div>
+  );
+}
