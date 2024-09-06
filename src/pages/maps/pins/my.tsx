6eb806e9218a823cf928e1b760453ef2bc4113f4
sheikhfoysaldiu <foysal15-3442@diu.edu.bd>
2024-09-06 22:01:42 +0600

    pin showing table

--- src/pages/maps/pins/my.tsx
+++ src/pages/maps/pins/my.tsx
@@ -1,4 +1,6 @@
 import { Location, LocationConsumer } from "@prisma/client";
+import { formatDate } from "date-fns";
+import Image from "next/image";
 import { useModal } from "~/components/hooks/use-modal-store";
 import { Button } from "~/components/shadcn/ui/button";
 import { api } from "~/utils/api";
@@ -13,36 +15,41 @@ export default function Page() {
 
 function ConsumedPins() {
   const consumedPins = api.maps.pin.getAUserConsumedPin.useQuery();
+  console.log(consumedPins);
   if (consumedPins.isLoading) return <p>Loading...</p>;
   if (consumedPins.isError) return <p>Error</p>;
 
   return (
-    <div>
-      <div>
-        <div className="overflow-x-auto">
-          <table className="table table-zebra">
-            {/* head */}
-            <thead>
-              <tr>
-                <th></th>
-                <th>Collected At</th>
-                <th>Pin title</th>
-                <th>Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              {consumedPins.data.map((pin) => {
-                return (
-                  <ClaimConsumedPin
-                    key={pin.id}
-                    pin={pin}
-                    location={pin.location}
-                  />
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
+    <div className=" overflow-x-auto p-4">
+      <div className="inline-block min-w-full overflow-hidden rounded-lg shadow">
+        <table className="min-w-full leading-normal">
+          <thead>
+            <tr>
+              <th className="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-gray-600">
+                Location
+              </th>
+              <th className="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-gray-600">
+                Approve Status
+              </th>
+              <th className="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-gray-600">
+                Closed At
+              </th>
+              <th className="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-gray-600">
+                LAT | LNG
+              </th>
+              <th className="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-gray-600">
+                Claim Status
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {consumedPins.data.map((pin, id) => {
+              return (
+                <ClaimConsumedPin key={id} pin={pin} location={pin.location} />
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     </div>
   );
@@ -52,26 +59,76 @@ function ConsumedPins() {
 function ClaimConsumedPin({
   pin,
   location,
+  key,
 }: {
   pin: LocationConsumer;
   location: Location;
+  key: number;
 }) {
   const { onOpen } = useModal();
   return (
-    <tr key={pin.id}>
-      <th>{}</th>
-      <td>{pin.createdAt.toDateString()}</td>
-      <td>{location.title}</td>
-      <td>
-        <ClaimButton />
-      </td>
-    </tr>
+    <>
+      <tr>
+        <td className="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+          <div className="flex items-center">
+            <div className="h-10 w-10 flex-shrink-0">
+              <Image
+                height={1000}
+                width={1000}
+                className="h-full w-full rounded-full object-contain"
+                src={location.image ?? "/images/icons/wadzzo.svg"}
+                alt=""
+              />
+            </div>
+            <div className="ml-3">
+              <p className="whitespace-no-wrap text-gray-900">
+                {formatTitle(location.title)}
+              </p>
+            </div>
+          </div>
+        </td>
+        <td className="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+          {location.approved ? (
+            <span className="relative me-2 inline-block rounded border border-green-400 bg-green-100 px-2.5 py-0.5  text-xs font-medium leading-tight text-green-800 dark:bg-gray-700 dark:text-green-400">
+              <span
+                aria-hidden
+                className="absolute inset-0 rounded-full bg-green-200 opacity-50"
+              ></span>
+              <span className="relative">Approved</span>
+            </span>
+          ) : (
+            <span className="relative me-2  inline-block rounded  border   border-indigo-400 bg-indigo-100 px-2.5 py-0.5 text-xs font-medium text-indigo-800 dark:bg-gray-700 dark:text-indigo-400">
+              <span
+                aria-hidden
+                className="absolute inset-0 rounded-full bg-indigo-100 opacity-50"
+              ></span>
+              <span className="relative">Pending</span>
+            </span>
+          )}
+        </td>
+        <td className="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+          <p className="whitespace-no-wrap text-gray-900">
+            {formatDate(new Date(location.endDate), "dd/MM/yyyy")}
+          </p>
+        </td>
+        <td className="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+          <p className="whitespace-no-wrap text-gray-900">
+            {location.latitude} | {location.longitude}
+          </p>
+        </td>
+        <td className="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+          <p className="whitespace-no-wrap text-gray-900">
+            <ClaimButton />
+          </p>
+        </td>
+      </tr>
+    </>
   );
 
   function ClaimButton() {
     if (pin.claimedAt) {
       return (
-        <Button className=" md:w-1/4" disabled>
+        <Button variant="secondary" className="" disabled>
           Claimed
         </Button>
       );
@@ -79,7 +136,7 @@ function ClaimConsumedPin({
       return (
         <Button
           variant="destructive"
-          className="px-6 font-bold md:w-1/4"
+          className="px-6 font-bold "
           onClick={() =>
             onOpen("claim pin", {
               location: location,
@@ -91,7 +148,15 @@ function ClaimConsumedPin({
         </Button>
       );
     } else {
-      return <p>Not claimable</p>;
+      return (
+        <p className="me-2 rounded border border-pink-400 bg-pink-100 px-2.5 py-0.5 text-center text-xs font-medium text-pink-800 dark:bg-gray-700 dark:text-pink-400">
+          Not claimable
+        </p>
+      );
     }
   }
+}
+
+function formatTitle(title: string) {
+  return title.length > 20 ? title.slice(0, 20) + "..." : title;
 }
