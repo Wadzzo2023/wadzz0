0da73c2ddfd539691739f11cdd33ee77e4acca80
sheikhfoysaldiu <sheikhfoysal2025@gmail.com>
2024-09-30 17:40:04 +0600

    header fix

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -13,24 +13,26 @@ interface HeaderProps {
 
 function Header(_props: HeaderProps) {
   return (
-    <header className="h-20 bg-base-100/20 px-2 py-4">
-      <div
-        className={"flex items-center justify-between gap-2 md:justify-center "}
-      >
-        <div className="flex ">
-          <div className="lg:hidden">
-            <Hamburger />
-          </div>
-          <Logo className="self hidden sm:flex" />
+    <header className="h-20 bg-base-100/20 px-2 py-4 md:px-6">
+      <div className="flex w-full items-center justify-between xl:hidden">
+        <div className={"flex items-center gap-2"}>
+          <Hamburger />
+          <Logo />
         </div>
-        <div className="self-end  md:hidden">
-          {/* <ConnectWalletButton  /> */}
+        {/* <ConnectWalletButton /> */}
+        <SiteAssetBalance />
+      </div>
+      <div className="hidden xl:flex">
+        <Image
+          className="rounded-box"
+          height={100}
+          width={100}
+          src={"/images/waddzo.gif"}
+          alt={process.env.NEXT_PUBLIC_ASSET_CODE?.toString() ?? ""}
+        />
 
-          <SiteAssetBalance />
-        </div>
-        <div className="ml-auto hidden md:flex">
-          <SiteAssetBalance />
-        </div>
+        <Logo />
+        <SiteAssetBalance />
       </div>
     </header>
   );
