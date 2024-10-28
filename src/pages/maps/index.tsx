d655a446e715795561ca5002a06f96ef61c6fd1c
Wadzzo <support@bandcoin.io>
2024-10-28 10:08:56 +0600

    map lanlat search

--- src/pages/maps/index.tsx
+++ src/pages/maps/index.tsx
@@ -5,10 +5,10 @@ import {
   Map,
   MapMouseEvent,
 } from "@vis.gl/react-google-maps";
-import { format } from "date-fns";
+import { format, set } from "date-fns";
 import { MapPin, Pin } from "lucide-react";
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Loading } from "react-daisyui";
 import toast from "react-hot-toast";
 import { useModal } from "~/components/hooks/use-modal-store";
@@ -41,11 +41,17 @@ function App() {
     lat: 22.54992,
     lng: 0,
   });
+  const [centerChanged, setCenterChanged] =
+    useState<google.maps.LatLngBoundsLiteral | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
   const [RangedPins, setRangedPins] = useState<pins>([]);
+  const [isCordsSearch, setIsCordsSearch] = useState<boolean>(false);
   const {
     selectedPlace: alreadySelectedPlace,
     setSelectedPlace: setAlreadySelectedPlace,
   } = useSelectedAutoSuggestion();
+  const [cordSearchCords, setCordSearchLocation] =
+    useState<google.maps.LatLngLiteral>();
   const [selectedPlace, setSelectedPlace] =
     useState<google.maps.places.PlaceResult | null>(null);
   const {
@@ -107,30 +113,60 @@ function App() {
     onSuccess: (data) => {
       console.log(data);
       setRangedPins(data);
+      setLoading(false);
     },
     onError: (error) => {
       toast.error(error.message);
+      setLoading(false);
     },
   });
-  const handleCenterChange = (center: google.maps.LatLngBoundsLiteral) => {
-    if (center) {
-      const data = GetPinMutation.mutate({
-        eastLongitude: center.east,
-        westLongitude: center.west,
-        northLatitude: center.north,
-        southLatitude: center.south,
-      });
+  const handleCenterChange = useCallback(
+    (center: google.maps.LatLngBoundsLiteral) => {
+      if (center) {
+        GetPinMutation.mutate({
+          eastLongitude: center.east,
+          westLongitude: center.west,
+          northLatitude: center.north,
+          southLatitude: center.south,
+        });
+      }
+    },
+    [GetPinMutation],
+  );
+  useEffect(() => {
+    if (centerChanged) {
+      setLoading(true);
+
+      // Start a 5-second timer to call handleCenterChange
+      const timer = setTimeout(() => {
+        console.log(
+          "Center hasn't changed for 5 seconds, calling handleCenterChange",
+        );
+        handleCenterChange(centerChanged);
+        setLoading(false);
+      }, 5000); // 5-second delay
+
+      // Clear the timer if centerChanged changes within 5 seconds, preventing handleCenterChange from being called
+      return () => {
+        clearTimeout(timer);
+        setLoading(false); // Reset loading if the center changes again within 5 seconds
+      };
     }
-  };
+  }, [centerChanged]); // Only depends on centerChanged, not handleCenterChange
 
   return (
     <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY!}>
-      <CustomMapControl controlPosition={2} onPlaceSelect={setSelectedPlace} />
+      <CustomMapControl
+        controlPosition={2}
+        onPlaceSelect={setSelectedPlace}
+        onCenterChange={setMapCenter}
+        setIsCordsSearch={setIsCordsSearch}
+        setCordSearchLocation={setCordSearchLocation}
+      />
       <Map
         onCenterChanged={(center) => {
-          console.log(center);
           setMapCenter(center.detail.center);
-          handleCenterChange(center.detail.bounds);
+          setCenterChanged(center.detail.bounds); // Update the centerChanged state with new bounds
         }}
         onZoomChanged={(zoom) => {
           setMapZoom(zoom.detail.zoom);
@@ -140,14 +176,35 @@ function App() {
         style={{ height: "100vh" }}
         defaultCenter={{ lat: 22.54992, lng: 0 }}
         defaultZoom={3}
-        onDragend={(center) => {
-          console.log(center);
-        }}
         zoom={mapZoom}
         center={mapCenter}
         gestureHandling={"greedy"}
         disableDefaultUI={true}
       >
+        {isCordsSearch && cordSearchCords && (
+          <AdvancedMarker
+            style={{
+              color: "red",
+            }}
+            position={{
+              lat: cordSearchCords.lat,
+              lng: cordSearchCords.lng,
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="size-8"
+            >
+              <path
+                fillRule="evenodd"
+                d="m7.539 14.841.003.003.002.002a.755.755 0 0 0 .912 0l.002-.002.003-.003.012-.009a5.57 5.57 0 0 0 .19-.153 15.588 15.588 0 0 0 2.046-2.082c1.101-1.362 2.291-3.342 2.291-5.597A5 5 0 0 0 3 7c0 2.255 1.19 4.235 2.292 5.597a15.591 15.591 0 0 0 2.046 2.082 8.916 8.916 0 0 0 .189.153l.012.01ZM8 8.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </AdvancedMarker>
+        )}
         <MyPins />
       </Map>
       <div className="hidden md:block">
@@ -162,46 +219,60 @@ function App() {
 
   function SideMapItem({ rangedPins }: { rangedPins: pins }) {
     return (
-      <div className="absolute bottom-4 right-4 top-4 flex w-64 items-center justify-center  ">
-        <div className="max-h-[300px] min-h-[300px] w-60 overflow-y-auto rounded-lg bg-white p-4  scrollbar-hide ">
+      <div className="absolute bottom-4 right-4 top-96 flex max-h-[400px] min-h-[400px] w-80  items-center justify-center">
+        <div className="max-h-[400px] min-h-[400px] w-80 overflow-y-auto rounded-lg bg-white p-4  scrollbar-hide ">
           <h2 className="mb-4 text-lg font-semibold">Nearby Locations</h2>
           <div className="space-y-4">
-            {rangedPins?.map((pin) => (
-              <div
-                onClick={() => {
-                  setAlreadySelectedPlace({
-                    lat: pin.latitude,
-                    lng: pin.longitude,
-                  });
-                }}
-                key={pin.id}
-                className="flex items-start gap-3 rounded-md bg-gray-50 p-3 shadow-lg transition-colors hover:bg-gray-100"
-              >
-                <MapPin className="h-5 w-5 flex-shrink-0 text-primary" />
-                <div className="flex-1">
-                  <h3 className="relative font-medium">
-                    {pin.title}{" "}
-                    <span className=" absolute bottom-4 right-0 text-[.60rem]">
-                      End At:
-                      {format(new Date(pin.endDate), "dd, yyyy")}
-                    </span>
-                  </h3>
-                  <div className="mt-1 flex items-center gap-1">
-                    <Avatar className="h-6 w-6">
-                      <Image
-                        width={24}
-                        height={24}
-                        src={pin.image ?? "/favicon.ico"}
-                        alt="Creator"
-                      />
-                    </Avatar>
-                    <Badge variant="secondary" className="text-xs">
-                      {pin._count.consumers} visitors
-                    </Badge>
+            {loading && (
+              <div className="flex h-[300px] items-center justify-center">
+                <Loading />
+              </div>
+            )}
+            {!loading && rangedPins.length <= 0 ? (
+              <div>
+                <h3 className="text-center text-gray-500">
+                  No nearby locations found
+                </h3>
+              </div>
+            ) : (
+              !loading &&
+              rangedPins?.map((pin) => (
+                <div
+                  onClick={() => {
+                    setAlreadySelectedPlace({
+                      lat: pin.latitude,
+                      lng: pin.longitude,
+                    });
+                  }}
+                  key={pin.id}
+                  className="flex items-start gap-3 rounded-md bg-gray-50 p-3 shadow-lg transition-colors hover:bg-gray-100"
+                >
+                  <MapPin className="h-5 w-5 flex-shrink-0 text-primary" />
+                  <div className="flex-1">
+                    <h3 className="relative font-medium">
+                      {pin.title}{" "}
+                      <span className=" absolute bottom-4 right-0 text-[.60rem]">
+                        End At:
+                        {format(new Date(pin.endDate), "dd, yyyy")}
+                      </span>
+                    </h3>
+                    <div className="mt-1 flex items-center gap-1">
+                      <Avatar className="h-6 w-6">
+                        <Image
+                          width={24}
+                          height={24}
+                          src={pin.image ?? "/favicon.ico"}
+                          alt="Creator"
+                        />
+                      </Avatar>
+                      <Badge variant="secondary" className="text-xs">
+                        {pin._count.consumers} visitors
+                      </Badge>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))
+            )}
           </div>
         </div>
       </div>
@@ -242,18 +313,15 @@ function App() {
                 setIsAutoCollect(pin.autoCollect); // Set isAutoCollect to true when a pin is clicked
               }}
             >
-              {pin._count.consumers < pin.limit ? (
-                <span>
-                  <Image
-                    src={pin.creator.profileUrl ?? "/favicon.ico"}
-                    width={30}
-                    height={30}
-                    alt="vong cong"
-                  />
-                </span>
-              ) : (
-                <span className="tree">(🌳)</span>
-              )}
+              <Image
+                src={pin.creator.profileUrl ?? "/favicon.ico"}
+                width={30}
+                height={30}
+                alt="Creator"
+                className={`h-10 w-10 bg-white ${
+                  !pin.autoCollect ? "rounded-full " : ""
+                } ${pin._count.consumers <= 0 ? "opacity-50" : "opacity-100"}`}
+              />
             </AdvancedMarker>
           ))}
         </>
