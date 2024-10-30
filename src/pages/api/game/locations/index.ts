185c6136709a4fa659a8c4c4c8c642a62c420425
Action <admin@actn.xyz>
2024-10-31 00:11:34 +0600

    update game locations api.

--- src/pages/api/game/locations/index.ts
+++ src/pages/api/game/locations/index.ts
@@ -1,9 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getSession } from "next-auth/react";
-import { db } from "~/server/db";
-import { Location } from "~/types/game/location";
-import { avaterIconUrl as abaterIconUrl } from "../brands";
+
 import NextCors from "nextjs-cors";
+import { z } from "zod";
+import { getToken } from "next-auth/jwt";
+import { db } from "~/server/db";
+import { avaterIconUrl as abaterIconUrl } from "../brands";
+import { Location } from "~/types/game/location";
 
 // import { getSession } from "next-auth/react";
 
@@ -17,129 +19,141 @@ export default async function handler(
     origin: "*",
     optionsSuccessStatus: 200,
   });
-  const session = await getSession({ req });
+  const token = await getToken({ req });
 
-  // Check if the user is authenticated
-  if (!session) {
+  if (!token) {
     res.status(401).json({
       error: "User is not authenticated",
     });
     return;
   }
+  const data = z.object({ filterId: z.string() }).safeParse(req.query); // Use req.query instead of req.body
 
-  // Return the locations
-
-  const all_creator_public_pin = await db.location.findMany({
-    select: {
-      id: true,
-      link: true,
-      latitude: true,
-      longitude: true,
-      description: true,
-      creatorId: true,
-      title: true,
-      limit: true,
-      _count: {
-        select: {
-          consumers: {
-            where: { userId: session.user.id },
+  if (!data.success) {
+    console.log("data.error", data.error);
+    return res.status(400).json({
+      error: data.error,
+    });
+  }
+  const userId = token.sub;
+  if (data.data.filterId === "1") {
+    const getAllFollowedBrand = await db.creator.findMany({
+      where: {
+        followers: {
+          some: {
+            userId: userId,
           },
         },
       },
-      image: true,
-      autoCollect: true,
-      creator: true,
-    },
-    where: {
-      approved: { equals: true },
-      endDate: { gte: new Date() },
-      tier: { is: null },
-    },
-  });
-
-  const user = await db.user.findUnique({
-    where: { id: session.user.id },
-    select: {
-      followings: {
-        include: {
-          creator: {
-            include: {
-              Location: {
-                include: {
-                  consumers: {
-                    select: { userId: true },
-                    where: { userId: session.user.id },
-                  },
-                  creator: true,
-                },
-                where: {
-                  approved: { equals: true },
-                  endDate: { gte: new Date() },
-                  tier: {
-                    isNot: null,
-                  },
-                },
-              },
+    });
+    // now i am extracting this brands pins
+    const pins = await db.location.findMany({
+      where: {
+        creatorId: {
+          in: getAllFollowedBrand.map((el) => el.id),
+        },
+        approved: { equals: true },
+        endDate: { gte: new Date() },
+        tier: { is: null },
+      },
+      select: {
+        id: true,
+        link: true,
+        latitude: true,
+        longitude: true,
+        description: true,
+        creatorId: true,
+        title: true,
+        limit: true,
+        _count: {
+          select: {
+            consumers: {
+              where: { userId: userId },
             },
           },
         },
+        image: true,
+        autoCollect: true,
+        creator: true,
       },
-    },
-  });
+    });
+    console.log("pins", pins.length);
 
-  const following_pins = user?.followings.map((el) => {
-    const pins = el.creator.Location.map((location) => location);
-    return pins;
-  });
+    const locations: Location[] = pins.map((location) => {
+      return {
+        id: location.id,
+        lat: location.latitude,
+        lng: location.longitude,
+        title: location.title,
+        description: location.description ?? "No description provided",
+        brand_name: location.creator.name,
+        url: location.link ?? "https://wadzzo.com/",
+        image_url:
+          location.image ?? location.creator.profileUrl ?? WadzzoIconURL,
+        collected: location._count.consumers >= location.limit,
+        collection_limit_remaining: location.limit - location._count.consumers,
+        auto_collect: location.autoCollect,
+        brand_image_url: location.creator.profileUrl ?? abaterIconUrl,
+        brand_id: location.creatorId,
+        public: true,
+      };
+    });
+    console.log("locations", locations.length);
 
-  const allFollowingPins = user?.followings.flatMap(
-    (el) => el.creator.Location,
-  );
+    res.status(200).json({ locations });
+  } else {
+    const all_creator_public_pin = await db.location.findMany({
+      select: {
+        id: true,
+        link: true,
+        latitude: true,
+        longitude: true,
+        description: true,
+        creatorId: true,
+        title: true,
+        limit: true,
+        _count: {
+          select: {
+            consumers: {
+              where: { userId: userId },
+            },
+          },
+        },
+        image: true,
+        autoCollect: true,
+        creator: true,
+      },
+      where: {
+        approved: { equals: true },
+        endDate: { gte: new Date() },
+        tier: { is: null },
+      },
+    });
+    const locations: Location[] = all_creator_public_pin.map((location) => {
+      return {
+        id: location.id,
+        lat: location.latitude,
+        lng: location.longitude,
+        title: location.title,
+        description: location.description ?? "No description provided",
+        brand_name: location.creator.name,
+        url: location.link ?? "https://wadzzo.com/",
+        image_url:
+          location.image ?? location.creator.profileUrl ?? WadzzoIconURL,
+        collected: location._count.consumers >= location.limit,
+        collection_limit_remaining: location.limit - location._count.consumers,
+        auto_collect: location.autoCollect,
+        brand_image_url: location.creator.profileUrl ?? abaterIconUrl,
+        brand_id: location.creatorId,
+        public: true,
+      };
+    });
+    console.log("locations", locations.length);
 
-  // let locations: Location[];
-  const followingsLocations = allFollowingPins?.map((location) => {
-    return {
-      id: location.id,
-      lat: location.latitude,
-      lng: location.longitude,
-      title: location.title,
-      description: location.description ?? "No description provided",
-      brand_name: location.creator.name,
-      url: location.link ?? "https://wadzzo.com/",
-      image_url: location.image ?? location.creator.profileUrl ?? WadzzoIconURL,
-      collected: location.consumers.length >= location.limit,
-      collection_limit_remaining: location.limit - location.consumers.length,
-      auto_collect: location.autoCollect,
-      brand_image_url: location.creator.profileUrl ?? abaterIconUrl,
-      brand_id: location.creatorId,
-      public: false,
-    };
-  });
-
-  let locations: Location[] = all_creator_public_pin.map((location) => {
-    return {
-      id: location.id,
-      lat: location.latitude,
-      lng: location.longitude,
-      title: location.title,
-      description: location.description ?? "No description provided",
-      brand_name: location.creator.name,
-      url: location.link ?? "https://wadzzo.com/",
-      image_url: location.image ?? location.creator.profileUrl ?? WadzzoIconURL,
-      collected: location._count.consumers >= location.limit,
-      collection_limit_remaining: location.limit - location._count.consumers,
-      auto_collect: location.autoCollect,
-      brand_image_url: location.creator.profileUrl ?? abaterIconUrl,
-      brand_id: location.creatorId,
-      public: true,
-    };
-  });
-
-  if (followingsLocations) {
-    locations = [...locations, ...followingsLocations];
+    res.status(200).json({
+      locations,
+    });
   }
-
-  res.status(200).json({ locations: locations });
 }
 
 export const WadzzoIconURL = "https://app.wadzzo.com/images/loading.png";
