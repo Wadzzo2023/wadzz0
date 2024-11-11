d2307a6ca967d17b1ef3ebff0a9f4a2ed4915298
Action <admin@actn.xyz>
2024-11-11 17:47:02 +0600

    auth

--- src/server/api-cors.ts
+++ src/server/api-cors.ts
@@ -1,12 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { headers } from "next/headers";
 import NextCors from "nextjs-cors";
 
 export async function EnableCors(req: NextApiRequest, res: NextApiResponse) {
   await NextCors(req, res, {
     // Options
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-    origin: "http://localhost:8081",
+    origin: "https://main.d20qrrwbkiopzh.amplifyapp.com",
     headers: ["Access-Control-Allow-Credentials"],
 
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
