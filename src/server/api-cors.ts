3be07a84cc26b5405a73c6ac3db97cb18366f30b
Action <admin@actn.xyz>
2024-11-11 18:20:01 +0600

    ...

--- src/server/api-cors.ts
+++ src/server/api-cors.ts
@@ -6,7 +6,6 @@ export async function EnableCors(req: NextApiRequest, res: NextApiResponse) {
     // Options
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     origin: "https://main.d20qrrwbkiopzh.amplifyapp.com",
-    headers: ["Access-Control-Allow-Credentials"],
 
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   });
