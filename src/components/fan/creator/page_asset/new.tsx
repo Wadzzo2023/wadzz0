09647b974258eefc3e6ea207e1b4aede29e35d78
Action <admin@actn.xyz>
2024-11-15 00:49:38 +0600

    ...

--- src/components/fan/creator/page_asset/new.tsx
+++ src/components/fan/creator/page_asset/new.tsx
@@ -86,7 +86,7 @@ function NewPageAssetFrom({ requiredToken }: { requiredToken: number }) {
     },
   });
 
-  // const assetAmount = api.fan.trx.getAssetNumberforXlm.useQuery();
+  // const assetAmount = api.fan.trx.getAssetNumberforXlm.useQuery();a
 
   const trxMutation = api.fan.trx.createCreatorPageAsset.useMutation({
     onSuccess: async (data) => {
