a83664f92a70cee65617b0ee6a88c8045fce68ea
Action <admin@actn.xyz>
2024-12-24 15:00:13 +0600

    fix next config image url problem with env name

--- next.config.js
+++ next.config.js
@@ -50,7 +50,7 @@ const config = {
       { hostname: "ipfs.io" },
       { hostname: "daisyui.com" },
       { hostname: "picsum.photos" },
-      { hostname: `${process.env.AWS_BUCKET_NAME}.s3.amazonaws.com` },
+      { hostname: `${process.env.NEXT_AWS_BUCKET_NAME}.s3.amazonaws.com` },
     ],
   },
 
