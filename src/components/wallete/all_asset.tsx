85bfd168a097997ef64b7a4cb3c4790e9b4d55cf
Foysal Sheikh <support@wadzzo.com>
2025-01-03 15:46:19 +0600

    homepage filtering

--- src/components/wallete/all_asset.tsx
+++ src/components/wallete/all_asset.tsx
@@ -58,7 +58,6 @@ export default function AllAsset() {
   if (assets.data ?? musicAssets.data ?? adminAssets.data ?? fanAssets.data)
     return (
       <div
-
         className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6"
       >
         {assets.data?.pages.map((page) =>
