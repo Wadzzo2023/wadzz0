31d0b812348390125204bbe7a665b7b674040895
Foysal Sheikh <admin@actn.xyz>
2025-01-09 16:51:28 +0600

    db user return

--- src/pages/api/game/user/index.ts
+++ src/pages/api/game/user/index.ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 import { getSession } from "next-auth/react";
 import { EnableCors } from "~/server/api-cors";
+import { db } from "~/server/db";
 
 export default async function handler(
   req: NextApiRequest,
@@ -12,8 +13,12 @@ export default async function handler(
     const session = await getSession({ req });
     console.log(session);
     if (session) {
-      // console.log(session.user);
-      return res.status(200).json(session.user);
+      const user = await db.user.findUnique({
+        where: {
+          id: session.user.id,
+        },
+      });
+      return res.status(200).json(user);
     } else {
       return res.status(401).json({ error: "Unauthorized" });
     }
