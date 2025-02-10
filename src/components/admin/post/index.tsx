6fbc237a1b3558b41886683a325a1b1c9f98eb36
IH <ahammedzonaid@gmail.com>
2025-02-10 19:49:04 +0600

    fix

--- src/components/admin/post/index.tsx
+++ src/components/admin/post/index.tsx
@@ -3,7 +3,7 @@ import DeletePost from "./delete-post";
 
 export default function PostDeleteComponent() {
   return (
-    <div className="my-2   bg-base-100 py-10">
+    <div className="my-2   bg-base-100 p-10">
       <h2 className="py-2 text-lg font-bold">Delete Post</h2>
       Paste post id to delete a post
       <DeletePost />
