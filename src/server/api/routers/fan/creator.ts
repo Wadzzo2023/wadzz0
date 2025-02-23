bea3081e9f2b78244d11dfcf63762e9511181c9d
Foysal Sheikh <support@action-tokens.com>
2025-02-23 23:18:15 +0600

    send + claim gift

--- src/server/api/routers/fan/creator.ts
+++ src/server/api/routers/fan/creator.ts
@@ -7,6 +7,7 @@ import { CreatorAboutShema } from "~/components/fan/creator/about";
 import { brandCreateRequestSchema } from "~/components/fan/creator/onboarding/create-form";
 import { MAX_ASSET_LIMIT } from "~/components/fan/creator/page_asset/new";
 import {
+  getCreatorShopAssetBalance,
   sendAssetXDRForAsset,
   sendAssetXDRForNative,
 } from "~/lib/stellar/fan/creator_pageasset_buy";
@@ -316,9 +317,15 @@ export const creatorRouter = createTRPCRouter({
           creatorPageAsset.issuer,
         );
         if (bal) {
-          return { balance: bal, asset: creatorPageAsset.code };
+          return {
+            balance: bal, assetCode: creatorPageAsset.code,
+            assetIssuer: creatorPageAsset.issuer
+          };
         } else {
-          return { balance: 0, asset: creatorPageAsset.code };
+          return {
+            balance: 0, assetCode: creatorPageAsset.code,
+            assetIssuer: creatorPageAsset.issuer
+          };
         }
       } else {
         if (creator.customPageAssetCodeIssuer) {
@@ -334,12 +341,31 @@ export const creatorRouter = createTRPCRouter({
           const bal = storageAcc.getTokenBalance(assetCode, assetIssuer.data);
 
           if (bal >= 0) {
-            return { balance: bal, asset: assetCode };
+            return { balance: bal, assetCode: assetCode, assetIssuer: assetIssuer.data };
           } else {
             throw new Error("Invalid asset code or issuer");
           }
         } else throw new Error("creator has no page asset");
       }
+    },
+  ),
+  getCreatorShopAssetBalance: creatorProcedure.query(
+    async ({ ctx }) => {
+      const creator = await ctx.db.creator.findUnique({
+        where: { id: ctx.session.user.id },
+
+      });
+
+      if (!creator) {
+        throw new Error("Creator not found");
+      }
+
+      const creatorStoragePub = creator.storagePub;
+
+      return await getCreatorShopAssetBalance({
+        creatorStoragePub,
+      });
+
     },
   ),
   getFansList: protectedProcedure.query(async ({ ctx }) => {
--- src/server/api/routers/fan/trx.ts
+++ src/server/api/routers/fan/trx.ts
@@ -26,7 +26,7 @@ import {
   createStorageTrxWithXLM,
 } from "~/lib/stellar/fan/create_storage";
 import { follow_creator } from "~/lib/stellar/fan/follow_creator";
-import { sendGift } from "~/lib/stellar/fan/send_gift";
+import { sendGift, sendGitfAsPlatformAsset } from "~/lib/stellar/fan/send_gift";
 import { trustCustomPageAsset } from "~/lib/stellar/fan/trust_custom_page_asset";
 import { StellarAccount } from "~/lib/stellar/marketplace/test/Account";
 import {
@@ -42,7 +42,11 @@ import {
 import { db } from "~/server/db";
 import { PaymentMethodEnum } from "~/components/BuyItem";
 import { useCreatorStorageAcc } from "~/lib/state/wallete/stellar-balances";
-
+enum assetType {
+  PAGEASSET = "PAGEASSET",
+  PLATFORMASSET = "PLATFORMASSET",
+  SHOPASSET = "SHOPASSET"
+}
 export const trxRouter = createTRPCRouter({
   createCreatorPageAsset: protectedProcedure
     .input(
@@ -303,10 +307,21 @@ export const trxRouter = createTRPCRouter({
     }),
 
   giftFollowerXDR: protectedProcedure // only logged creator can do that
-    .input(FanGitFormSchema)
+    .input(z.object({
+      pubkey: z.string().length(56),
+      amount: z.number({
+        required_error: "Amount is required",
+        invalid_type_error: "Amount must be a number",
+        message: "Amount must be a number",
+      }).int().positive(),
+      assetCode: z.string().nonempty(),
+      assetIssuer: z.string().nonempty(),
+      assetType: z.nativeEnum(assetType),
+      signWith: SignUser,
+    }))
     .mutation(async ({ input, ctx }) => {
       const creatorId = ctx.session.user.id;
-      const pubkey = input.pubkey;
+      const { pubkey, amount, assetCode, assetIssuer } = input
 
       const isFollower = await ctx.db.follow.findUnique({
         where: {
@@ -322,21 +337,66 @@ export const trxRouter = createTRPCRouter({
         where: { id: creatorId },
         include: { pageAsset: true },
       });
+      const { storageSecret } = creator;
 
-      if (creator.pageAsset) {
-        const { code, issuer } = creator.pageAsset;
+      if (input.assetType === assetType.PAGEASSET) {
+        if (creator.pageAsset) {
+          const { code, issuer } = creator.pageAsset;
+          console.log(code, issuer)
+          // send email
 
-        // send email
-        const { storageSecret } = creator;
-
+          return await sendGift({
+            customerPubkey: pubkey,
+            creatorPageAsset: { code, issuer },
+            creatorStorageSec: storageSecret,
+            creatorPub: creatorId,
+            price: input.amount,
+            signWith: input.signWith,
+          });
+        } else if (creator.customPageAssetCodeIssuer) {
+          const [code, issuer] = creator.customPageAssetCodeIssuer.split("-");
+          const issuerVal = z.string().length(56).safeParse(issuer);
+          if (issuerVal.success && code) {
+            const { storageSecret } = creator;
+            return await sendGift({
+              customerPubkey: pubkey,
+              creatorPageAsset: { code, issuer: issuerVal.data },
+              creatorStorageSec: storageSecret,
+              creatorPub: creatorId,
+              price: input.amount,
+              signWith: input.signWith,
+            });
+          } else {
+            throw new Error("Issuer is invalid");
+          }
+        }
+        else {
+          throw new Error("creator has no page asset");
+        }
+      }
+      else if (input.assetType === assetType.PLATFORMASSET) {
+        return await sendGitfAsPlatformAsset({
+          reciver: pubkey,
+          creatorId: creatorId,
+          amount: amount,
+          assetCode: assetCode,
+          assetIssuer: assetIssuer,
+          signWith: input.signWith,
+        })
+      }
+      else if (input.assetType === assetType.SHOPASSET) {
         return await sendGift({
           customerPubkey: pubkey,
-          creatorPageAsset: { code, issuer },
+          creatorPageAsset: { code: assetCode, issuer: assetIssuer },
           creatorStorageSec: storageSecret,
           creatorPub: creatorId,
           price: input.amount,
+          signWith: input.signWith,
         });
-      } else throw new Error("creator has no page asset");
+      }
+
+
+
     }),
 
   getRequiredPlatformAsset: publicProcedure
