eae3438f4cd7deb3d5b5eaf3966b6a0e8bf36639
Wadzzo <support@wadzzo.com>
2025-11-13 00:01:55 +0600

    nav gurd added

--- src/pages/fans/creator/layout.tsx
+++ src/pages/fans/creator/layout.tsx
@@ -1,4 +1,6 @@
 import { useRouter } from "next/router";
+import { CreatorNavigation } from "~/components/fan/nav/creator-mode";
+import { creatorExtraFiledsSchema } from "~/types/creator";
 import { api } from "~/utils/api";
 
 export default function CreatorLayout({
@@ -12,6 +14,18 @@ export default function CreatorLayout({
 
   if (!creator.data?.approved) {
     router.push("/");
+  }
+
+  // Check navigation permissions
+  const extraFields = creatorExtraFiledsSchema.parse(creator.data?.extraFields);
+  const currentPath = router.pathname;
+  const navEntry = Object.values(CreatorNavigation).find((nav) =>
+    currentPath.startsWith(nav.path),
+  );
+
+  if (navEntry?.needAproval && !extraFields?.navPermission) {
+    router.push("/fans/creator/settings");
+    // return null;
   }
 
   return <>{children}</>;
