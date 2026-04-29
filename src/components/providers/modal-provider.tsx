6d8203dee6a2fbbf41e1e6269e3081782f23bd44
fyron alias <support@wadzzo.com>
2026-04-29 12:28:31 +0600

    fix: remove unused MapModalComponent import and update BASE_URL to development environment

--- src/components/providers/modal-provider.tsx
+++ src/components/providers/modal-provider.tsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from "react";
 import SendAssets from "../modals/send-assets-modal";
 import ReceiveAssetsModal from "../modals/receive-assets-modal";
 import AddAssets from "../modals/add-asset-modal";
-import MapModalComponent from "../modals/map-modal";
 import CopyCutPinModal from "../modals/copy-cut-pin-modal";
 import ClaimPinModal from "../modals/claim-pin-modal";
 import ShareModal from "../modals/share-modal";
